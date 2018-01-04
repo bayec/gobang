b9bab074753cdabf3c78501d7d3ef52b152148ab
lichen <lichen@sunniwell.net>
2018-01-04 11:38:38 +0800

    代码规范化

--- gobang/js/main.js
+++ gobang/js/main.js
@@ -1,60 +1,60 @@
-var text = document.getElementsByClassName('text');
+var clientWidth = document.documentElement.clientWidth;  //变量暂未使用，预留
 
-//定义二维数组作为棋盘
-var maps = new Array(15);
-var len = maps.length;
-
-for(var i = 0; i < len; i++){
-    maps[i] = new Array();
-    for(var j = 0;j < len; j++){
-        maps[i][j] = 0;
+/*定义一个二维数组，作为15X15的棋盘*/
+var cbArray = new Array(15);
+for(var i = 0; i < cbArray.length; i++){
+    cbArray[i] = new Array(cbArray.length);
+    for(var j = 0;j < cbArray.length; j++){
+        cbArray[i][j] = 0;
     }
 }
 
-//初始化棋子
+/*初始化棋子*/
 var black = new Image();
 var white = new Image();
-var clientWidth = document.documentElement.clientWidth;  //变量暂未使用
-
 black.src = "images/black.png";
 white.src = "images/white.png";
 
-//棋盘初始化
-var can = document.getElementById('can');
-var ctx = can.getContext("2d");  //获取该canvas的2D绘图环境对象
-ctx.strokeStyle = "#333";
+/*棋盘初始化*/
+var canvasOfMain = document.getElementById('main');
+var ctxOfMain = canvasOfMain.getContext("2d");  //获取该canvas的2D绘图环境对象
+ctxOfMain.strokeStyle = "#333";
 
 //下面这四行绘制正方形，对于15X15的棋盘，横竖绘制14个正方形即可
-for(var m = 0;m < len - 1; m++){
-    for(var n = 0; n < len - 1; n++){
-        ctx.strokeRect(m*40+40, n*40+40, 40, 40);  //绘制40的小正方形
+for(var m = 0;m < cbArray.length - 1; m++){
+    for(var n = 0; n < cbArray.length - 1; n++){
+        ctxOfMain.strokeRect(m*40+40, n*40+40, 40, 40);  //绘制40的小正方形
     }
 }
 
-//绘制文字，控制顶部状态栏
-var can1 = document.getElementsByClassName('text');
-var ctx1 = can1[0].getContext("2d");
+/*棋盘状态标志位*/
+var isBlack = true; //当前棋子是否为黑色
+var isGameOver = false; //当前局是否结束，预留变量
 
-ctx1.beginPath();
-ctx1.font=("100px Georgia");
-ctx1.fillStyle="#F70707";
-//ctx1.fillText("Hello",40,100);
+/*底部状态栏初始化*/
+var canvasOfStatusBar = document.getElementsByClassName('statusbar');
+var ctxOfStatusBar = canvasOfStatusBar[0].getContext("2d");
 
-var isBlack = true;
+ctxOfStatusBar.beginPath();
+ctxOfStatusBar.font=("100px Georgia");
+ctxOfStatusBar.fillStyle="#F70707";
+//ctxOfStatusBar.fillText("Hello",40,100);
 
-//下子
-can.onclick=function play(e){
-    //获取棋盘偏移量
+/*点击棋盘开始下棋*/
+canvasOfMain.onclick = function play(e){
+    console.log("鼠标点击的坐标(" + e.clientX + "," + e.clientY + ")");
+
+    //获取棋盘相对外边框左边和顶边的偏移量，即确定棋盘左上角那个点的坐标
     var l = this.offsetLeft + 40;
     var t = this.offsetTop + 40;
-    console.log("点击的坐标("+e.clientX+","+e.clientY+")");
-        //获取点击相对棋盘坐标
+
+    //获取点击的位置相对棋盘左上角那个点的坐标的偏移量
     var x = e.clientX - l;
     var y = e.clientY - t;
 
-    var row = 0, col = 0, index = 0;
+    var row = 0, col = 0;
 
-    /*确定棋子真实坐标，左上角为(0,0)，右下角为(14,14)*/
+    //确定棋子二维数组坐标，左上角为(0,0)，右下角为(14,14)
     if( x > 0 ){
         if( x % 40 < 20 ){
             col = parseInt(x/40);
@@ -71,112 +71,107 @@ can.onclick=function play(e){
         }
     }
 
-    console.log("棋盘坐标("+col+","+row+")");
+    console.log("棋盘坐标(" + col + "," + row + ")");
 
-    if( maps[row][col] === 0 ){
+    if( cbArray[row][col] === 0 ){
         if(isBlack){//下黑子
-            ctx.drawImage(black, col*40+22, row*40+22);//理论上是加20，这里加22属于微调，让棋子摆在正中央
+            ctxOfMain.drawImage(black, col*40+22, row*40+22);//理论上是加20，这里加22属于微调，让棋子摆在正中央
             isBlack = false;
-            maps[row][col] = 1; //黑子为1
-            iswin(1, row, col);
+            cbArray[row][col] = 1; //黑子为1
+            check(1, row, col);
         }else{
-            ctx.drawImage(white, col*40+22, row*40+22);
+            ctxOfMain.drawImage(white, col*40+22, row*40+22);
             isBlack = true;
-            maps[row][col] = 2; //白子为2
-            iswin(2, row, col);
-        }
-    }
-
-    function iswin(t,row,col){
-        var orgrow = 0, orgcol = 0, total = 0;
-        reset();
-        // alert(total);
-
-        //判断每行是否有五个
-        while( col > 0 && maps[row][col-1] === t ){  //当前子左边还有
-            total++;
-            col--;
-        }
-        row = orgrow;
-        col = orgcol;
-        while( col+1<15 && maps[row][col+1] === t ){  //当前子右边还有
-            col++;
-            total++;
-        }
-        // alert(total);
-        celebrate();
-
-        //判断每列是否有五个
-        reset();
-
-        while(row>0&&maps[row-1][col]===t){   //当前子上面还有
-            total++;
-            row--;
-        }
-        row = orgrow;
-        col = orgcol;
-        while(row+1<15&&maps[row+1][col]===t){  //下面
-            total++;
-            row++;
-        }
-        celebrate();
-
-        //左上 右下有没有五个
-        reset();
-        while(row>0&&col>0&&maps[row-1][col-1]===t){ //左上1
-            row--;
-            col--;
-            total++;
-        }
-        row = orgrow;
-        col = orgcol;
-        while(row+1<15&&col+1<15&&maps[row+1][col+1]===t){  //右下1
-            row++;
-            col++;
-            total++;
-        }
-        // alert(total)
-        celebrate();
-
-        //左下 右上有没有五个
-        reset();
-        // alert(total);
-        while(row>0&&col+1<15&&maps[row-1][col+1]===t){  //右上
-            row--;
-            col++;
-            total++;
-        }
-        row = orgrow;
-        col = orgcol;
-        while(row+1<15&&col>0&&maps[row+1][col-1]===t){   //左下
-            row++;
-            col--;
-            total++;
-        }
-        // alert(total);
-        celebrate();
-
-        function celebrate(){       //显示哪边赢
-            if(total>=5){
-                if( t === 1 ){
-                    // alert("黑子赢");
-                    // text[0].innerHTML="黑子赢";
-                    // cxt1.clearRect(0,0,can1.width,can1.height);
-                    ctx1.clearRect(0,0,can1[0].width,can1[0].height);
-                    ctx1.fillText("黑子赢",0,100);
-                }else{
-                    // alert("白子赢");
-                    // text[0].innerHTML="白子赢";
-                    // cxt1.clearRect(0,0,can1.width,can1.height);
-                    ctx1.clearRect(0,0,can1[0].width,can1[0].height);
-                    ctx1.fillText("白子赢",0,100);
-                }
-            }
-        }
-        function reset(){
-            orgrow = row;
-            orgcol = col;
-            total = 1;
+            cbArray[row][col] = 2; //白子为2
+            check(2, row, col);
         }
     }
 };
+
+/*判断棋局是否结束*/
+function check(color, row, col){
+    var rowBak = row, colBak = col, total = 1;
+
+    //判断东西方向（←→）是否有五个
+    while( col > 0 && cbArray[row][col-1] === color ){  //当前棋子左边还有几个同色的棋子
+        total++;
+        col--;
+    }
+    row = rowBak;
+    col = colBak;
+    while( col+1<15 && cbArray[row][col+1] === color ){  //当前棋子右边还有几个同色的棋子
+        col++;
+        total++;
+    }
+    isWin();
+
+    //判断南北方向（↑↓）是否有五个
+    reset();
+    while( row > 0 && cbArray[row-1][col] === color ){   //当前棋子上边还有几个同色的棋子
+        total++;
+        row--;
+    }
+    row = rowBak;
+    col = colBak;
+    while( row+1 < 15 && cbArray[row+1][col] === color ){  //当前棋子下边还有几个同色的棋子
+        total++;
+        row++;
+    }
+    isWin();
+
+    //判断东南和西北方向（↖↘）是否有五个
+    reset();
+    while( row > 0 && col>0 && cbArray[row-1][col-1] === color ){ //当前棋子左上边还有几个同色的棋子
+        row--;
+        col--;
+        total++;
+    }
+    row = rowBak;
+    col = colBak;
+    while( row+1 < 15 && col+1 < 15 && cbArray[row+1][col+1] === color ){  //当前棋子右下边还有几个同色的棋子
+        row++;
+        col++;
+        total++;
+    }
+    isWin();
+
+    //判断东北和西南方向（↙↗）有五个
+    reset();
+    while( row > 0 && col+1 < 15 && cbArray[row-1][col+1] === color ){  //当前棋子右上边还有几个同色的棋子
+        row--;
+        col++;
+        total++;
+    }
+    row = rowBak;
+    col = colBak;
+    while( row+1 < 15 && col > 0 && cbArray[row+1][col-1] === color ){   //当前棋子左下边还有几个同色的棋子
+        row++;
+        col--;
+        total++;
+    }
+    isWin();
+
+    //判断是否结束
+    function isWin(){
+        if( total >= 5 ){
+            if( color === 1 ){
+                // alert("黑子赢");
+                // canvasOfStatusBar[0].innerHTML="黑子赢";
+                ctxOfStatusBar.clearRect(0, 0, canvasOfStatusBar[0].width, canvasOfStatusBar[0].height);
+                ctxOfStatusBar.fillText("黑子赢", 0, 100);
+            }else{
+                // alert("白子赢");
+                // canvasOfStatusBar[0].innerHTML="白子赢";
+                ctxOfStatusBar.clearRect(0, 0, canvasOfStatusBar[0].width, canvasOfStatusBar[0].height);
+                ctxOfStatusBar.fillText("白子赢", 0, 100);
+            }
+        }
+    }
+
+    //重置当前棋子的坐标和连子数
+    function reset(){
+        rowBak = row;
+        colBak = col;
+        total = 1;
+    }
+}
