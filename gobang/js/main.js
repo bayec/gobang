ce5b452feb2928961a59ffbf9d288f3d20e9f257
lichen <lichen@sunniwell.net>
2018-01-05 17:55:49 +0800

    修改描述如下： 1.增加悔棋功能 2.优化界面布局，适应低分辨率的屏幕

--- gobang/js/main.js
+++ gobang/js/main.js
@@ -5,7 +5,11 @@ console.log(clientWidth + "X" + clientHeight);
 /*定义一个二维数组a[15][15]，保存当前棋盘状态*/
 var curCbArray = arrayInit(15);
 
-/*定义一个三维数组，保存每下一步的棋盘状态*/
+/*定义一个二维数组，保存前一次的棋盘状态*/
+var lastCbArray = arrayInit(15);
+
+/*定义一个二维数组，保存前两次的棋盘状态*/
+var llastCbArray = arrayInit(15);
 
 /*棋子初始化*/
 var black = new Image();
@@ -22,54 +26,74 @@ drawChessboard(ctxOfMain);
 /*棋盘状态标志位*/
 var isBlack = true; //当前棋子是否为黑色
 var isGameOver = false; //当前局是否结束
+var hasPiece = 0; //当前棋盘有几颗棋子
+var revokeFlag = 0;    //悔棋标志位，大于等于2才能悔棋
 
 /*状态栏初始化*/
 var canvasOfStatusBar = document.getElementsByClassName('statusbar');
 var ctxOfStatusBar = canvasOfStatusBar[0].getContext("2d");
 
 ctxOfStatusBar.beginPath();
-ctxOfStatusBar.font=("100px Georgia");
-ctxOfStatusBar.fillStyle="#F70707";
+ctxOfStatusBar.font = ("100px Georgia");
+ctxOfStatusBar.fillStyle = "#F70707";
 
 /*二维数组初始化*/
-function arrayInit(size){
+function arrayInit(size) {
     var arr = new Array(size);
-    for(var i = 0; i < size; i++){
+    for (var i = 0; i < size; i++) {
         arr[i] = new Array(size);
-        for(var j = 0;j < size; j++){
+        for (var j = 0; j < size; j++) {
             arr[i][j] = 0;
         }
     }
     return arr;
 }
 
+/*二维数组间复制*/
+function arrayCopy(dst, src) {
+    for (var i = 0; i < 15; i++) {
+        for (var j = 0; j < 15; j++) {
+            dst[i][j] = src[i][j];
+        }
+    }
+}
+
+/*二维数组清空*/
+function arrayReset(src) {
+    for (var i = 0; i < 15; i++) {
+        for (var j = 0; j < 15; j++) {
+            src[i][j] = 0;
+        }
+    }
+}
+
 /*绘制棋盘*/
-function drawChessboard(ctxOfMain){
+function drawChessboard(ctxOfMain) {
     //下面这四行绘制正方形，对于15X15的棋盘，横竖绘制14个正方形即可
-    for(var i = 0; i < curCbArray.length - 1; i++){
-        for(var j = 0; j < curCbArray.length - 1; j++){
-            ctxOfMain.strokeRect(i*40+40, j*40+40, 40, 40);  //绘制40像素的正方形
+    for (var i = 0; i < curCbArray.length - 1; i++) {
+        for (var j = 0; j < curCbArray.length - 1; j++) {
+            ctxOfMain.strokeRect(i * 36 + 36, j * 36 + 36, 36, 36);  //绘制40像素的正方形
         }
     }
 
     /*绘制棋盘上的五个点*/
-    ctxOfMain.fillRect(3*40+37, 3*40+37, 6, 6);
-    ctxOfMain.fillRect(3*40+37, 11*40+37, 6, 6);
-    ctxOfMain.fillRect(11*40+37, 3*40+37, 6, 6);
-    ctxOfMain.fillRect(7*40+37, 7*40+37, 6, 6);
-    ctxOfMain.fillRect(11*40+37, 11*40+37, 6, 6);
+    ctxOfMain.fillRect(3 * 36 + 33, 3 * 36 + 33, 6, 6);
+    ctxOfMain.fillRect(3 * 36 + 33, 11 * 36 + 33, 6, 6);
+    ctxOfMain.fillRect(11 * 36 + 33, 3 * 36 + 33, 6, 6);
+    ctxOfMain.fillRect(7 * 36 + 33, 7 * 36 + 33, 6, 6);
+    ctxOfMain.fillRect(11 * 36 + 33, 11 * 36 + 33, 6, 6);
 
     return ctxOfMain;
 }
 
 /*点击棋盘开始下棋*/
-canvasOfMain.onclick = function click(e){
+canvasOfMain.onclick = function click(e) {
     console.log("鼠标点击的坐标(" + e.clientX + "," + e.clientY + ")");
 
-    if( isGameOver === false ){
+    if (isGameOver === false) {
         //获取棋盘相对外边框左边和顶边的偏移量，即确定棋盘左上角那个点的坐标
-        var l = this.offsetLeft + 40;
-        var t = this.offsetTop + 40;
+        var l = this.offsetLeft + 36;
+        var t = this.offsetTop + 36;
 
         //获取点击的位置相对棋盘左上角那个点的坐标的偏移量
         var x = e.clientX - l;
@@ -78,40 +102,58 @@ canvasOfMain.onclick = function click(e){
         var row = 0, col = 0;
 
         //确定棋子二维数组坐标，左上角为(0,0)，右下角为(14,14)
-        if( x > 0 ){
-            if( x % 40 < 20 ){
-                row = parseInt(x/40);
-            }else{
-                row = parseInt(x/40)+1;
+        if (x > 0) {
+            if (x % 36 < 18) {
+                row = parseInt(x / 36);
+            } else {
+                row = parseInt(x / 36) + 1;
             }
         }
 
-        if( y > 0 ){
-            if( y % 40 < 20 ){
-                col = parseInt(y/40);
-            }else{
-                col = parseInt(y/40)+1;
+        if (y > 0) {
+            if (y % 36 < 18) {
+                col = parseInt(y / 36);
+            } else {
+                col = parseInt(y / 36) + 1;
             }
         }
 
         console.log("棋盘坐标(" + row + "," + col + ")");
         drop(col, row); //棋盘横竖和二维数组的行列需要反一下
-    }else{
-       toast("比赛结束，请点击重新开始！");
+    } else {
+        toast("比赛结束，请点击重新开始！");
     }
 };
 
 /*落子*/
-function drop(row, col){
-    if( curCbArray[row][col] === 0 ){
-        if(isBlack){//下黑子
-            ctxOfMain.drawImage(black, col*40+22, row*40+22);//理论上是加20，这里加22属于微调，让棋子摆在正中央
+function drop(row, col) {
+    if (curCbArray[row][col] === 0) {
+        if (isBlack) {//下黑子
+
+            ctxOfMain.drawImage(black, col * 36 + 18, row * 36 + 18);//理论上是加20，这里加22属于微调，让棋子摆在正中央
             isBlack = false;
+            hasPiece++;
+            revokeFlag++;
+            if( revokeFlag >= 2 ){
+                var button = document.getElementById('revoke');
+                button.disabled = false;
+            }
+            arrayCopy(llastCbArray, lastCbArray);
+            arrayCopy(lastCbArray, curCbArray);
             curCbArray[row][col] = 1; //黑子为1
             check(1, row, col);
-        }else{
-            ctxOfMain.drawImage(white, col*40+22, row*40+22);
+        } else {
+
+            ctxOfMain.drawImage(white, col * 36 + 18, row * 36 + 18);
             isBlack = true;
+            hasPiece++;
+            revokeFlag++;
+            if( revokeFlag >= 2 ){
+                var button1 = document.getElementById('revoke');
+                button1.disabled = false;
+            }
+            arrayCopy(llastCbArray, lastCbArray);
+            arrayCopy(lastCbArray, curCbArray);
             curCbArray[row][col] = 2; //白子为2
             check(2, row, col);
         }
@@ -119,17 +161,17 @@ function drop(row, col){
 }
 
 /*判断棋局是否结束*/
-function check(color, row, col){
+function check(color, row, col) {
     var rowBak = row, colBak = col, total = 1;
 
     //判断东西方向（←→）是否有五个
-    while( col > 0 && curCbArray[row][col-1] === color ){  //当前棋子左边还有几个同色的棋子
+    while (col > 0 && curCbArray[row][col - 1] === color) {  //当前棋子左边还有几个同色的棋子
         total++;
         col--;
     }
     row = rowBak;
     col = colBak;
-    while( col+1<15 && curCbArray[row][col+1] === color ){  //当前棋子右边还有几个同色的棋子
+    while (col + 1 < 15 && curCbArray[row][col + 1] === color) {  //当前棋子右边还有几个同色的棋子
         col++;
         total++;
     }
@@ -137,13 +179,13 @@ function check(color, row, col){
 
     //判断南北方向（↑↓）是否有五个
     reset();
-    while( row > 0 && curCbArray[row-1][col] === color ){   //当前棋子上边还有几个同色的棋子
+    while (row > 0 && curCbArray[row - 1][col] === color) {   //当前棋子上边还有几个同色的棋子
         total++;
         row--;
     }
     row = rowBak;
     col = colBak;
-    while( row+1 < 15 && curCbArray[row+1][col] === color ){  //当前棋子下边还有几个同色的棋子
+    while (row + 1 < 15 && curCbArray[row + 1][col] === color) {  //当前棋子下边还有几个同色的棋子
         total++;
         row++;
     }
@@ -151,14 +193,14 @@ function check(color, row, col){
 
     //判断东南和西北方向（↖↘）是否有五个
     reset();
-    while( row > 0 && col>0 && curCbArray[row-1][col-1] === color ){ //当前棋子左上边还有几个同色的棋子
+    while (row > 0 && col > 0 && curCbArray[row - 1][col - 1] === color) { //当前棋子左上边还有几个同色的棋子
         row--;
         col--;
         total++;
     }
     row = rowBak;
     col = colBak;
-    while( row+1 < 15 && col+1 < 15 && curCbArray[row+1][col+1] === color ){  //当前棋子右下边还有几个同色的棋子
+    while (row + 1 < 15 && col + 1 < 15 && curCbArray[row + 1][col + 1] === color) {  //当前棋子右下边还有几个同色的棋子
         row++;
         col++;
         total++;
@@ -167,14 +209,14 @@ function check(color, row, col){
 
     //判断东北和西南方向（↙↗）有五个
     reset();
-    while( row > 0 && col+1 < 15 && curCbArray[row-1][col+1] === color ){  //当前棋子右上边还有几个同色的棋子
+    while (row > 0 && col + 1 < 15 && curCbArray[row - 1][col + 1] === color) {  //当前棋子右上边还有几个同色的棋子
         row--;
         col++;
         total++;
     }
     row = rowBak;
     col = colBak;
-    while( row+1 < 15 && col > 0 && curCbArray[row+1][col-1] === color ){   //当前棋子左下边还有几个同色的棋子
+    while (row + 1 < 15 && col > 0 && curCbArray[row + 1][col - 1] === color) {   //当前棋子左下边还有几个同色的棋子
         row++;
         col--;
         total++;
@@ -182,16 +224,16 @@ function check(color, row, col){
     isWin();
 
     //判断是否结束
-    function isWin(){
-        if( total >= 5 ){
-            if( color === 1 ){
+    function isWin() {
+        if (total >= 5) {
+            if (color === 1) {
                 // alert("黑子赢");
                 // canvasOfStatusBar[0].innerHTML="黑子赢";
                 ctxOfStatusBar.clearRect(0, 0, canvasOfStatusBar[0].width, canvasOfStatusBar[0].height);
                 ctxOfStatusBar.fillText("黑子赢", 0, 100);
                 isGameOver = true;
                 // toast("比赛结束，黑子获胜！");
-            }else{
+            } else {
                 // alert("白子赢");
                 // canvasOfStatusBar[0].innerHTML="白子赢";
                 ctxOfStatusBar.clearRect(0, 0, canvasOfStatusBar[0].width, canvasOfStatusBar[0].height);
@@ -203,7 +245,7 @@ function check(color, row, col){
     }
 
     //重置当前棋子的坐标和连子数
-    function reset(){
+    function reset() {
         rowBak = row;
         colBak = col;
         total = 1;
@@ -211,11 +253,17 @@ function check(color, row, col){
 }
 
 /*重新开始*/
-function restart(){
-     for(var i = 0; i < curCbArray.length; i++){
-        for(var j = 0;j < curCbArray.length; j++){
-            if( curCbArray[i][j] !== 0 ) {
+function restart() {
+    for (var i = 0; i < 15; i++) {
+        for (var j = 0; j < 15; j++) {
+            if (curCbArray[i][j] !== 0) {
                 curCbArray[i][j] = 0;
+            }
+            if (lastCbArray[i][j] !== 0) {
+                lastCbArray[i][j] = 0;
+            }
+            if (llastCbArray[i][j] !== 0) {
+                llastCbArray[i][j] = 0;
             }
         }
     }
@@ -227,30 +275,63 @@ function restart(){
     ctxOfStatusBar.clearRect(0, 0, canvasOfStatusBar[0].width, canvasOfStatusBar[0].height);
     isGameOver = false;
     isBlack = true;
+    hasPiece = 0;
 }
 
 /*悔棋*/
 function revoke() {
+    //重新画棋盘
+    ctxOfMain.clearRect(0, 0, 640, 640);
+    drawChessboard(ctxOfMain);
 
+    //重新摆子
+    for (var i = 0; i < 15; i++) {
+       for (var j = 0; j < 15; j++) {
+           if( llastCbArray[i][j] === 1 ){
+               ctxOfMain.drawImage(black, j * 36 + 18, i * 36 + 18);
+           }else if( llastCbArray[i][j] === 2 ) {
+               ctxOfMain.drawImage(white, j * 36 + 18, i * 36 + 18);
+           }
+       }
+   }
+
+    /*回退当前棋盘状态，清空前两次棋盘状态*/
+    arrayCopy(curCbArray, llastCbArray);
+    arrayReset(llastCbArray);
+    arrayReset(lastCbArray);
+
+    /*减两颗棋子*/
+    if( hasPiece <= 1 ){
+        isBlack = true;
+        hasPiece = 0;
+    }else if( hasPiece >= 2 ){
+        hasPiece -= 2;
+    }
+
+    revokeFlag = 0;
+
+    /*悔棋按钮禁止按，因为只能悔一步*/
+    var button = document.getElementById('revoke');
+    button.disabled = true;
 }
 
 /*toast提示*/
-function toast(msg){
-    setTimeout(function(){
-        document.getElementsByClassName('toast-wrap')[0].getElementsByClassName('toast-msg')[0].innerHTML=msg;
+function toast(msg) {
+    setTimeout(function () {
+        document.getElementsByClassName('toast-wrap')[0].getElementsByClassName('toast-msg')[0].innerHTML = msg;
         var toastTag = document.getElementsByClassName('toast-wrap')[0];
-        toastTag.className = toastTag.className.replace('toastAnimate','');
-        setTimeout(function(){
+        toastTag.className = toastTag.className.replace('toastAnimate', '');
+        setTimeout(function () {
             toastTag.className = toastTag.className + ' toastAnimate';
         }, 100);
-    },500);
+    }, 500);
 }
 
 /*人机模式下判断用户选择的是黑子还是白子*/
-function selectColor(){
+function selectColor() {
     var radio = document.getElementsByName("radio-color");
-    for(var i = 0; i < radio.length; i++) {
-        if( radio[i].checked ){
+    for (var i = 0; i < radio.length; i++) {
+        if (radio[i].checked) {
             //TODO
         }
     }
